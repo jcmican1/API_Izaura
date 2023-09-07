d6ebe1e91260d1adc335b600767ed6b650091d7e
Hol1sPruev4Sen4 <juliand_triana@soy.sena.edu.co>
2023-09-07 16:59:42 -0500

    feat: Se unen todas la rutas
    Se unen todos los enrutadores hechos por cada integrante en un solo proyecto

--- BackendAPi_Productos/index.js
+++ BackendAPi_Productos/index.js
@@ -21,12 +21,19 @@ app.listen(PUERTO, () => {
 
 
 // Importa los enrutadores
-const plantillaProductoRouter = require('./routers/plantillaProductoRouter');
+
 const estadosRouter = require('./routers/estadosRouter');
 const rolesRouter = require('./routers/rolesRouter');
 const usuariosRouter = require('./routers/usuariosRouter');
 const notificacionesRouter = require('./routers/notificacionesRouter');
 const usuarioNotificacionesRouter = require('./routers/usuarioNotificacionesRouter');
+
+const plantillaProductoRouter = require('./routers/plantillaProductoRouter');
+const unidad_medidaRouter = require('./routers/unidad_medidaRouter');
+const plantilla_producto_has_producto_materia_prima = require('./routers/plantilla_producto_has_producto_materia_prima');
+const categoriaRouter = require('./routers/categoriaRouter');
+const producto_materia_prima = require('./routers/producto_materia_prima');
+
 const proveedorRouter = require('./routers/proveedorRouter');
 const ubicacionAlmacenRouter = require('./routers/ubicacionAlmacenRouter');
 const existenciasRouter = require('./routers/existenciasRouter');
@@ -34,12 +41,18 @@ const movimientoRouter = require('./routers/movimientoRouter');
 const motivoRouter = require('./routers/motivoRouter');
 
 // Usa los enrutadores
-app.use('/plantillaproducto', plantillaProductoRouter);
 app.use('/estados', estadosRouter);
 app.use('/roles', rolesRouter);
 app.use('/usuarios', usuariosRouter);
 app.use('/notificaciones', notificacionesRouter);
 app.use('/usuario-notificaciones', usuarioNotificacionesRouter);
+
+app.use('/plantillaproducto', plantillaProductoRouter);
+app.use('./unidad-medida', unidad_medidaRouter);
+app.use('./producto-materia', plantilla_producto_has_producto_materia_prima);
+app.use('/categoria', categoriaRouter);
+app.use('./materia-prima', producto_materia_prima);
+
 app.use('/proveedor', proveedorRouter);
 app.use('/ubicacion-almacen/', ubicacionAlmacenRouter);
 app.use('/existencias', existenciasRouter);
